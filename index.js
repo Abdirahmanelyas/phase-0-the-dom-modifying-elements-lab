18b8f23508ded0c496041d53366b7a5decc9cd52
Abdirahmanelyas <abdirahman.elyas@student.moringaschool.com>
2024-07-17 11:33:31 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+
+newHeader.textContent = 'YOUR-NAME is the champion'; 
+
+
+document.body.appendChild(newHeader);
